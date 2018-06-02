d10c04f4c1df52dd943a48ea0586b6c2c3cc3242
Muhammad Usman <muhammad@usmanity.com>
2018-06-01 23:40:09 -0700

    removing cormorant for now, going with a sans-serif font, Inter UI for now

--- header.php
+++ header.php
@@ -9,5 +9,4 @@
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
     <?php wp_head(); ?>
-    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,400,500,600" rel="stylesheet">
 </head>
