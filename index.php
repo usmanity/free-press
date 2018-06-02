5ef8da1455d8c421c495e06dde87e5d3f069109d
Muhammad Usman <muhammad@usmanity.com>
2018-06-02 12:52:51 -0700

    add title with permalink on index page

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php get_header(); ?>
 
-<div class="container">
+<div class="container main-page">
     <!-- START WORDPRESS LOOP -->
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div class="post">
+            <a href="<?php echo esc_url( get_permalink()); ?>" class="title-link">
+                <h1 class="title">
+                    <?php the_title(); ?>
+                </h1>
+            </a>
             <?php the_content(); ?>
         </div>
     <?php endwhile; else : ?>
